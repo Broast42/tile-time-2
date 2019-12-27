44647c89fde9f22632373df81b774039543930a2
rdowd <rjarrettdowd@yahoo.com>
2019-12-26 21:01:40 -0500

    beginning to build header component.

---
+++ js/index.js
@@ -0,0 +1,23 @@
+
+//header component
+    // <header>
+    //     <div class="header-div">
+    //         <h1>Tile Time Gaming</h1>
+    //         <nav class="main-nav">
+    //             <div class="link">Link</a>
+    //
+    //         </nav>
+    //     </div>
+    //     <div class="games-nav">
+    //         <nav>
+    //             <a href="#">GAME</a>
+    //             
+    //         </nav>
+    //     </div>
+        
+    // </header>
+
+
+    
+
+
